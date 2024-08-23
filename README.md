1437ee10d6537576b52f6e6ad1cbd152b0dfd674
dozyio <github@dozy.io>
2024-08-23 12:31:52 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Peer ID Generator
+
+Generates private key, public key and associated peer id for use with libp2p.
+Outputs to json
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"flag"
+	"fmt"
+
+	"github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/libp2p/go-libp2p/core/peer"
+)
+
+type KeysPeerID struct {
+	PrivKey string `json:"private_key"`
+	PubKey  string `json:"public_key"`
+	PeerID  string `json:"peer_id"`
+}
+
+type KeysPeers struct {
+	Hosts []KeysPeerID `json:"hosts"`
+}
+
+func main() {
+	c := flag.Int("c", 1, "number of keys to generate")
+
+	flag.Parse()
+
+	keysPeers := KeysPeers{
+		Hosts: []KeysPeerID{},
+	}
+
+	for i := 0; i < *c; i++ {
+		keysPeers.Hosts = append(keysPeers.Hosts, generate())
+	}
+
+	out, err := json.MarshalIndent(keysPeers, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s", out)
+}
+
+func generate() KeysPeerID {
+	priv, pub, err := crypto.GenerateKeyPair(crypto.Ed25519, -1)
+	if err != nil {
+		panic(err)
+	}
+
+	mPriv, err := crypto.MarshalPrivateKey(priv)
+	if err != nil {
+		panic(err)
+	}
+
+	mPub, err := crypto.MarshalPublicKey(pub)
+	if err != nil {
+		panic(err)
+	}
+
+	id, err := peer.IDFromPublicKey(pub)
+	if err != nil {
+		panic(err)
+	}
+
+	keysPeerID := KeysPeerID{
+		PubKey:  hex.EncodeToString(mPub),
+		PrivKey: hex.EncodeToString(mPriv),
+		PeerID:  id.String(),
+	}
+
+	return keysPeerID
+}
